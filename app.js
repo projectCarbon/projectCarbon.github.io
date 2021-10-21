ef57c6ffb6a87d204e0260f63ecb91f4391b2ca5
projectCarbon <92950383+projectCarbon@users.noreply.github.com>
2021-10-22 00:53:19 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,14 @@
+let btn = document.getElementsByClassName("menu-btn")[0]
+let menu = document.getElementsByClassName("hidden-menu")[0]
+
+btn.addEventListener('click', ()=>{
+    if (menu.style.opacity === "0"){
+        menu.style.opacity = "1"
+    }
+
+    else{
+        menu.style.opacity = "0"
+    }
+})
+
+
